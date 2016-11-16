198fd005113d7f36677fdd0e79f1eba47178bfc8
TJRobson <thomas.j.robson@hotmail.com>
2016-11-17 00:01:28 +1300

    set react-router, with es6

---
+++ app/config/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ReactRouter, IndexRoute, Router, Route, hashHistory } from 'react-router'
+
+import Main from '../components/Main'
+import Home from '../components/Home'
+
+
+const routes = (
+  <Router history={hashHistory}>
+    <Route path='/' component={Main} >
+      <IndexRoute component={Home} />
+    </Route>
+  </Router>
+)
+
+
+export default routes
--- app/index.js
+++ app/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-class HelloWorld extends React.Component {
+import routes from './config/routes'
 
-  render(){
-    return ( <div>
-      <h1>Hello World!</h1>
-      </div>
-    )
-  }
-}
 
-ReactDom.render(<HelloWorld />, document.getElementById('app'))
+ReactDom.render(
+  routes,
+  document.getElementById('app')
+)
