6a53bd9e29cdc7924c954d02b699076d37d5441d
TJRobson <thomas.j.robson@hotmail.com>
2016-12-02 22:50:48 +1300

    finished video 7

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,7 @@
-var React = require('react')
-var ConfirmBattle = require('../components/ConfirmBattle')
+const React = require('react')
+const ConfirmBattle = require('../components/ConfirmBattle')
+const githubHelpers = require('../utils/githubHelpers')
+
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -13,7 +15,13 @@ var ConfirmBattleContainer = React.createClass({
   },
   componentDidMount: function () {
     var query = this.props.location.query;
-    // Fetch info from github then update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+    .then(function (players){
+      this.setState({
+        isLoading: false,
+        playersInfo: [players[0], players[1]]
+      })
+    }.bind(this))
   },
   render: function () {
     return (
