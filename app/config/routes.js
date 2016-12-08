f6e606da478ef7eb2f4e85f023cdbce53cca9f81
TJRobson <thomas.j.robson@hotmail.com>
2016-12-08 22:37:25 +1300

    finished video 9-s exercises

--- app/config/routes.js
+++ app/config/routes.js
@@ -5,6 +5,7 @@ import Main from '../components/Main'
 import Home from '../components/Home'
 import PromptContainer from '../containers/PromptContainer'
 import ConfirmBattleContainer from '../containers/ConfirmBattleContainer'
+import ResultsContainer from '../containers/ResultsContainer'
 
 
 const routes = (
@@ -14,6 +15,7 @@ const routes = (
       <Route path='playerOne' header='Player One' component={PromptContainer} />
       <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
       <Route path='battle' component={ConfirmBattleContainer} />
+      <route path='results' component={ResultsContainer} />
     </Route>
   </Router>
 )
